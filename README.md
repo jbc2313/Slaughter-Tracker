0cbbf1494d853591ca31cd382a8c11bba756baaa
jb corbin <101614021+jbc2313@users.noreply.github.com>
2024-10-24 19:40:01 -0500

    Name and type of animal entry is working

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 # Slaughter-Tracker
 Slaughter Tracker to keep track of animals slaughtered.
-=======
+=========================================================
 * How to run the file. *
 
 **space ff in neovim to find and search files **
@@ -9,4 +8,6 @@ Slaughter Tracker to keep track of animals slaughtered.
 cc main.c -o test
 
 ./test
->>>>>>> 53488cd (First commit, basic setup complete)
+
+* Debugger
+    - use LLDB
--- main.c
+++ main.c
@@ -2,36 +2,38 @@
 #include <stdlib.h>
 
 
-//needs moved to another file
-void GetAnimalInfo(){
-// function needs to return a true or false so program can continue or retry
-    
-    char animalOwner[20];
-    char userInput[5];
-    int numButcher;
-    char checkBool[1];
+//needs moved to another file?
 
+// this function needs to return an array of the info gathered.
+void GetAnimalInfo(char *uInput, int nButcher, char *cBool){
+    // function needs to return a true or false so program can continue or retry
 
     // ask Y or N
     // end of loop if correct.
 
-    printf("Enter a name for who the animal belongs too.");
-    scanf("%s", animalOwner);
     printf("Please enter the type of animal: Pig or Bovine\n");
-    scanf("%s", userInput);
+    scanf("%s", uInput);
     printf("Please enter the number of animals to be slaughtered?\n");
-    scanf("%d", &numButcher); 
-    printf("You dropped of %d %s to be slaughtered?\n", numButcher, userInput);
+    scanf("%d", &nButcher); 
+    printf("You dropped of %d %s to be slaughtered?\n", nButcher, uInput);
     printf("Is that correct?\n");
-    scanf("%s", checkBool);
+    scanf("%s", cBool);
+
 }
 
-
 int main(){
+    char userInput[5];
+    int numButcher;
+    char checkBool[1];
+
+    char animalOwner[20];
 
     printf("Welcome to Woodward Meats Slaughter Tracker.\n");
 
-     
+    printf("Enter a name for who the animal belongs too.\n");
+    scanf("%s", animalOwner);
+
+    GetAnimalInfo(userInput, numButcher, checkBool);
 
     // ask for their name, animal type, and number of animals.
     // confirm inputs, if incorrect go back through the loop and reask the questions.
